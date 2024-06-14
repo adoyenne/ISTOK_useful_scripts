59a959cdb89a03f7bd668d24bde5b2f6646a1d0b
adoyenne <a.doyennel@vu.nl>
2024-06-14 22:04:10 +0200

    В шаблоне HTML сделана  фильтрация appointments.filter(date=day, time=hour).exists() и в представлении добавлен метод get_object_or_404 для получения специалиста по ID, чтобы убедиться, что специалист существует.

--- calendar_views.py
+++ calendar_views.py
@@ -1,33 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Specialist, Appointment
+from django.db.models import Q
+from .models import Appointment
+from users.models import User
 from datetime import date, timedelta
 
 @login_required
 def calendar_view(request):
-    specialists = Specialist.objects.all()
-    appointments = Appointment.objects.all()  # Get all appointments
+    """
+    Представление для записи на встречу с сотрудником
+    """
+    work_time_list = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"]
+    specialists = User.objects.filter(groups__name__in=['Consultants', 'Designers', 'Measurers'])
+    appointments = Appointment.objects.all()
     query = request.GET.get('q')
     specialist_type = request.GET.get('type')
 
     if query:
-        specialists = specialists.filter(last_name__icontains=query)
+        specialists = specialists.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(patronymic__icontains=query))
     if specialist_type:
-        specialists = specialists.filter(specialist_type=specialist_type)
+        specialists = specialists.filter(groups__name__in=[specialist_type])
 
     if request.method == "POST":
         specialist_id = request.POST.get('specialist')
         appointment_date = request.POST.get('date')
         appointment_time = request.POST.get('time')
-        specialist = Specialist.objects.get(id=specialist_id)
-        # Check if the slot is already booked
-        if not Appointment.objects.filter(specialist=specialist, date=appointment_date, time=appointment_time).exists():
-            Appointment.objects.create(user=request.user, specialist=specialist, date=appointment_date, time=appointment_time, is_booked=True)
+        specialist = get_object_or_404(User, id=specialist_id)
+        if not Appointment.objects.filter(staff_user=specialist, date=appointment_date, time=appointment_time).exists():
+            Appointment.objects.create(client_user=request.user, staff_user=specialist, date=appointment_date, time=appointment_time, is_booked=True)
         return redirect('calendar_view')
 
     context = {
+        'work_time_list': work_time_list,
         'specialists': specialists,
         'appointments': appointments,
         'days': [date.today() + timedelta(days=i) for i in range(30)],  # Next 30 days
     }
-    return render(request, 'scheduler/calendar.html', context)
+    return render(request, 'calendar.html', context=context)
